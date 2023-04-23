08c7131940378cf6a74dd0750423d9f74944f835
bluff <bluffyx91@gmail.com>
2023-04-23 20:45:18 +0300

    added react-markup library and integrated it into the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+
 function App() {
-  return <div></div>
+  const [markdown, setMarkdown] = useState('## markdown preview')
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        >
+          {markdown}
+        </textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  )
 }
 
 export default App
